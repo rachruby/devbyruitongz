1846ad1d7d5f5be08463cbb66b0a45000485c85e
Rubi <75260480+rachruby@users.noreply.github.com>
2025-03-18 13:21:41 -0400

    styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/skillBg.png";
+import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
