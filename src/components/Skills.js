f34ffbd61681da9c2a4e4b7d39fd17c4ea9a54e5
Rubi <75260480+rachruby@users.noreply.github.com>
2025-03-18 15:59:57 -0400

    styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/skillsLogs.png"
+import colorSharp from "../assets/img/bglog.png"
 
 export const Skills = () => {
   const responsive = {
